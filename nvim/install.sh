7fe2cf3ccf123952d16e8726f3947954667de480
Samasaur1 <30577766+Samasaur1@users.noreply.github.com>
2022-08-20 15:45:10 -0400

    [nvim] Correctly link snippets dir

--- nvim/install.sh
+++ nvim/install.sh
@@ -5,4 +5,5 @@ cd ~
 # Vim
 mkdir -p .config/nvim
 ln -s ~/dotfiles/nvim/init.lua .config/nvim/init.lua
+ln -s ~/dotfiles/nvim/snippets .config/nvim/snippets
 nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
