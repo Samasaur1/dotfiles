2c9cfc16accf699bbd4309108c18f3815b5df621
Samasaur1 <30577766+Samasaur1@users.noreply.github.com>
2022-09-29 22:28:31 -0700

    [nvim] Start with spellchecking on for txt and md files

--- nvim/install.sh
+++ nvim/install.sh
@@ -6,5 +6,6 @@ cd ~
 mkdir -p .config/nvim
 ln -s ~/dotfiles/nvim/init.lua .config/nvim/init.lua
 ln -s ~/dotfiles/nvim/snippets .config/nvim/snippets
+ln -s ~/dotfiles/nvim/ftplugin .config/nvim/ftplugin
 ln -s ~/dotfiles/nvim/spell .config/nvim/spell
 nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
