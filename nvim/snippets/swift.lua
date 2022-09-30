28f5240290f259c23c77923baaa3d19c76f82eb0
Samasaur1 <30577766+Samasaur1@users.noreply.github.com>
2022-09-29 22:25:07 -0700

    [nvim] Add more Swift snippets

--- nvim/snippets/swift.lua
+++ nvim/snippets/swift.lua
@@ -24,7 +24,11 @@ return {
         t({"", "}", ""}),
         i(0),
     }),
-    s("printv", { --print variable=<value>
+    s({
+        trig="printv",
+        name="printv-standalone",
+        dscr="Print [type here]=<value>",
+    }, {
         t("print(\""),
         rep(1),
         t("=\\("),
@@ -42,5 +46,50 @@ return {
         i(2),
         t({"", "}", ""}),
         i(0),
+    }),
+    s({
+        trig="printv",
+        name="printv-selection",
+        dscr="Print 'selected variable'=<value>",
+    }, {
+        t('print("'),
+        f(function(_, snip)
+            -- TM_SELECTED_TEXT is a table to account for multiline-selections.
+            --          -- In this case only the first line is inserted.
+            return snip.env.TM_SELECTED_TEXT[1] or {}
+        end, {}),
+        t('"=\\('),
+        f(function(_, snip)
+            -- TM_SELECTED_TEXT is a table to account for multiline-selections.
+            --          -- In this case only the first line is inserted.
+            return snip.env.TM_SELECTED_TEXT[1] or {}
+        end, {}),
+        t(')")')
+    }),
+    s({
+        trig="print",
+        name="print-selection",
+        dscr="Print 'selected variable'",
+    }, {
+        t('print('),
+        f(function(_, snip)
+            -- TM_SELECTED_TEXT is a table to account for multiline-selections.
+            --          -- In this case only the first line is inserted.
+            return snip.env.TM_SELECTED_TEXT[1] or {}
+        end, {}),
+        t(')')
+    }),
+    s({
+        trig="dump",
+        name="dump-selection",
+        dscr="Dump 'selected variable'",
+    }, {
+        t('dump('),
+        f(function(_, snip)
+            -- TM_SELECTED_TEXT is a table to account for multiline-selections.
+            --          -- In this case only the first line is inserted.
+            return snip.env.TM_SELECTED_TEXT[1] or {}
+        end, {}),
+        t(')')
     }),
 }
