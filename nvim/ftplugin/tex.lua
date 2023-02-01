fe527900336e3d61c39aef45ec36228083572fe2
Sam <30577766+Samasaur1@users.noreply.github.com>
2023-02-01 15:25:56 -0800

    [nvim] Add keybinding to compile LaTeX with SyncTeX

--- nvim/ftplugin/tex.lua
+++ nvim/ftplugin/tex.lua
@@ -1,3 +1,4 @@
 local opts = { noremap = true, silent = true }
 
 vim.api.nvim_buf_set_keymap(0, 'n', '<leader>lj', ":silent !/Applications/Skim.app/Contents/SharedSupport/displayline <C-r>=line('.')<CR> %<.pdf<CR>", opts)
+vim.api.nvim_buf_set_keymap(0, 'n', '<leader>lm', ":!pdflatex -synctex=1 %<CR>", opts)
