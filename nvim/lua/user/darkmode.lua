a26cbefa31dde9e4e0b57b5a4da06295e9747753
Sam <30577766+Samasaur1@users.noreply.github.com>
2023-06-16 11:03:34 -0700

    [nvim] Add cross-platform default for reading dark mode

--- nvim/lua/user/darkmode.lua
+++ nvim/lua/user/darkmode.lua
@@ -1,1 +1,17 @@
-vim.cmd('set background=dark')
+-- vim.cmd('set background=light')
+local f = io.popen("uname", "r")
+local s = f:read("*a")
+f:close()
+if s == "Darwin\n" then
+  local handle = io.popen("defaults read -globalDomain AppleInterfaceStyle &> /dev/null && printf dark || printf light")
+  local result = handle:read("*a")
+  handle:close()
+  vim.o.bg=result
+else
+  local dm = os.getenv("DARKMODE")
+  if dm == "NO" then
+    vim.o.bg="light"
+  else
+    vim.o.bg="dark"
+  end
+end
