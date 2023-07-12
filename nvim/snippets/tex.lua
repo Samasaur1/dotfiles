6d6137eeadd3c13e31e99647512d2f7a5b27fe5e
Sam <30577766+Samasaur1@users.noreply.github.com>
2023-07-12 11:00:12 -0700

    [nvim/snippets] Reformat TeX snippets

--- nvim/snippets/tex.lua
+++ nvim/snippets/tex.lua
@@ -6,37 +6,37 @@ function no_selected_text()
   return not selected_text_exists()
 end
 return {
-    s({
-        trig="bold",
-        name="tex_bold_selection",
-        dscr="Embolden selected TeX text",
+  s({
+    trig="bold",
+    name="tex_bold_selection",
+    dscr="Embolden selected TeX text",
     condition=no_selected_text,
     show_condition=no_selected_text,
-    }, {
-        t("\\textbf{"),
-        f(function(_, snip)
-            -- TM_SELECTED_TEXT is a table to account for multiline-selections.
-            --          -- In this case only the first line is inserted.
-            return snip.env.TM_SELECTED_TEXT[1] or {}
-        end, {}),
-        t("}"),
-        i(0),
+  }, {
+      t("\\textbf{"),
+      f(function(_, snip)
+        -- TM_SELECTED_TEXT is a table to account for multiline-selections.
+        --          -- In this case only the first line is inserted.
+        return snip.env.TM_SELECTED_TEXT[1] or {}
+      end, {}),
+      t("}"),
+      i(0),
     }),
-    s({
-        trig="italics",
-        name="tex_italics_selection",
-        dscr="Italicize selected TeX text",
+  s({
+    trig="italics",
+    name="tex_italics_selection",
+    dscr="Italicize selected TeX text",
     condition=no_selected_text,
     show_condition=no_selected_text,
-    }, {
-        t("\\emph{"),
-        f(function(_, snip)
-            -- TM_SELECTED_TEXT is a table to account for multiline-selections.
-            --          -- In this case only the first line is inserted.
-            return snip.env.TM_SELECTED_TEXT[1] or {}
-        end, {}),
-        t("}"),
-        i(0),
+  }, {
+      t("\\emph{"),
+      f(function(_, snip)
+        -- TM_SELECTED_TEXT is a table to account for multiline-selections.
+        --          -- In this case only the first line is inserted.
+        return snip.env.TM_SELECTED_TEXT[1] or {}
+      end, {}),
+      t("}"),
+      i(0),
     }),
   s({
     trig="bold",
